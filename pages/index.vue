dbe41b462e32db6285ad009d5eb72633540ba5d6
Clément Lemoine <clementlemoine.cl@gmail.com>
2020-01-30 16:45:50 +0100

    🎉

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div />
+    </div>
+</template>
+
+<script>
+import { getHome } from '~/cms';
+
+export default {
+    async asyncData() {
+        return {};
+    }
+};
+</script>
+
+<style lang="scss" scoped></style>
